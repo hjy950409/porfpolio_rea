dfdf3983bda511bcb4c063776615fa0eb79536f4
hjy950409 <hjy950409@naver.com>
2021-09-06 17:18:46 +0900

    수정수정수정중

--- docs/work1/index.js
+++ docs/work1/index.js
@@ -1,4 +1,6 @@
+
 console.clear();
+
 /*풀페이지*/ 
 function Fullpage__init() {
   new fullpage('#fullpage', {
@@ -97,3 +99,24 @@ $(document).ready(function() {
     }
   });
 });
+/*연락처*/
+function sendEmailForm(form) {
+  if ( form._replyto.value.length == 0 ) {
+    alert('이메일 주소를 입력해주세요.');
+    form._replyto.focus();
+    return;
+  }
+  
+  if ( form.message.value.length == 0 ) {
+    alert('메세지를 입력해주세요.');
+    form.message.focus();
+    return;
+  }
+  
+  form.submit();
+  
+  form._replyto.value = '';
+  form.message.value = '';
+  form.submit1.innerHTML = '전송되었습니다.';
+  form.submit1.disabled = true;
+}
